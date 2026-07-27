dc1528d84cefe970de4616e9f15074f7966abf09
Pranita Panchal <136261094+pranitap123@users.noreply.github.com>
2026-07-27 12:06:45 +0530

    feat(arrays-hashing): solve LeetCode 217 Contains Duplicate

---
+++ arrays-hashing/0217-contains-duplicate/solution.ts
@@ -0,0 +1,13 @@
+function containsDuplicate(nums: number[]): boolean {
+    const set = new Set<number>();
+
+    for (const num of nums) {
+        if (set.has(num)) {
+            return true;
+        }
+
+        set.add(num);
+    }
+
+    return false;
+}
